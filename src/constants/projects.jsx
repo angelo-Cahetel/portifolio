5608d36c114df845a663c1c88be789f924d673e0
Angelo Mendes <angelo030825@gmail.com>
2025-08-18 18:08:42 -0300

    criando a pagina de projetos e adicionando a foto do projeto 'peace'

---
+++ src/constants/projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <section className="flex w-[100%] min-h-[100vh] p-6 sticky">
+        <div className="bg-white rounded-2xl flex flex-col w-[100%] p-20">
+            <div className="justify-center">
+            <h1 className="text-[#0000FF] text-end text-8xl font-medium">Projects</h1>
+            </div>
+        </div>
+        </section>
+    );
+}
+
+export default Projects;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Header from "./constants/header.jsx";
+import Projects from "./constants/projects.jsx";
 import Contact from "./constants/Contact.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
     <App />
+    <Projects />
     <Contact />
   </StrictMode>
 );
