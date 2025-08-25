f6b3811b2ea302df83f9a349b34c147f1b412718
Angelo Mendes <angelo030825@gmail.com>
2025-08-25 15:40:33 -0300

    adicionando outros projetos

--- src/constants/projects.jsx
+++ src/constants/projects.jsx
@@ -1,27 +1,69 @@
 import React from "react";
 import Peace from "../assets/img/peace_project.jpg";
+import Poliverso from "../assets/img/poliverso.jpg";
 
 function Projects() {
-    return (
-        <section className="flex w-[100%] min-h-[100vh] p-6 sticky">
-        <div className="bg-white rounded-2xl flex flex-col w-[100%] p-20">
-            <div className="justify-center">
-            <h1 className="text-[#0000FF] text-end text-8xl font-medium">Projects</h1>
-            </div>
-            <div className="flex">
-                <div className="w-91 h-[516px] bg-white rounded-[20px] shadow-[4px_4px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
-                    <img className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]" src={ Peace } alt="" />
-                    <div>
-                        <h2 className="pl-5 text-4xl text-[#0000ff]">peace</h2>
-                        <p className="p-5">Peace é um aplicativo para iOS que desenvolvi com o objetivo de ajudar as pessoas a se organizarem melhor, gerenciarem suas tarefas e acompanharem sua rotina de forma prática.</p>
-                    </div>
-                </div>
-                <div></div>
-                <div></div>
-            </div>
+  return (
+    <section className="flex w-[100%] min-h-[100vh] p-6 sticky">
+      <div className="bg-white rounded-2xl flex flex-col w-[100%] p-20">
+        <div className="justify-center">
+          <h1 className="text-[#0000FF] text-end text-8xl font-medium">
+            Projects
+          </h1>
         </div>
-        </section>
-    );
+        <div className="flex gap-[45px]">
+          {/* peace */}
+          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[15px_15px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
+            <img
+              className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
+              src={Peace}
+              alt=""
+            />
+            <div>
+              <h2 className="pl-5 text-4xl text-[#0000ff]">peace</h2>
+              <p className="p-5">
+                Peace é um aplicativo para iOS que desenvolvi com o objetivo de
+                ajudar as pessoas a se organizarem melhor, gerenciarem suas
+                tarefas e acompanharem sua rotina de forma prática.
+              </p>
+            </div>
+          </div>
+
+          {/* poliverso */}
+          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[15px_15px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
+            <img
+              className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
+              src={Poliverso}
+              alt=""
+            />
+            <div>
+              <h2 className="pl-5 text-4xl text-[#0000ff]">poliveso</h2>
+              <p className="p-5">
+                A Poliverso é uma agencia do audiovisual que que conecta
+                talentos para produzir conteúdos de alta qualidade.
+              </p>
+            </div>
+          </div>
+
+          {/* outro projeto */}
+          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[15px_15px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
+            <img
+              className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
+              src={Poliverso}
+              alt=""
+            />
+            <div>
+              <h2 className="pl-5 text-4xl text-[#0000ff]">Poliveso</h2>
+              <p className="p-5">
+                A Poliverso é uma agencia do audiovisual que que conecta
+                talentos para produzir conteúdos de alta qualidade.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
