fe0b4c0687b4b01448b8e5c1cf96b2e9d1942f2a
Angelo Mendes <angelo030825@gmail.com>
2025-10-12 00:38:30 -0300

    crusor funcionando do jeito correto

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -2,32 +2,32 @@ import React, { useState, useEffect } from "react";
 import cursor from "../assets/img/cursor.svg";
 
 const CustomCursor = () => {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-    useEffect(() => {
-        const handleMouseMove = (e) => {
-            setPosition({ x: e.pageX, y: e.pageY });
-        };
-        window.addEventListener("mousemove", handleMouseMove);
-        return () => window.removeEventListener("mousemove", handleMouseMove);
-    }, []);
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.pageX, y: e.pageY });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
 
-    return (
-        <div className="absolute">
-            <img 
-            src={cursor} 
-            alt="Imagem que segue o cursor" 
-            className="pointer-events-none fixed z-50"
-            style={{
-                left: position.x,
-                top: position.y,
-                // transform: "translate(50%, 50%)",
-            }}
-            width={100}
-            height={100}
-            />
-        </div>
-    );
+  return (
+    <img
+      src={cursor}
+      alt="Imagem que segue o cursor"
+      style={{
+        position: "fixed",
+        left: position.x,
+        top: position.y,
+        transform: "translate(-50%, -50%)",
+        pointerEvents: "none",
+        zIndex: 9999,
+        width: 80,
+        height: 80,
+      }}
+    />
+  );
 };
 
 export default CustomCursor;
