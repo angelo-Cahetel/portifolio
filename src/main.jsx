a983ebb27e55ebfed18c7a92d5c02b5982c98dc0
Angelo Mendes <angelo030825@gmail.com>
2025-10-17 10:41:15 -0300

    resolvendo problema de importação do Header

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Header from "./constants/header.jsx";
+import Header from "./constants/Header.jsx";
 import Projects from "./constants/projects.jsx";
 import Contact from "./constants/Contact.jsx";
 
