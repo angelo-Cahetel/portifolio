ec1e2039b6c0b6ef3afc29f83e49739692930b69
Angelo Mendes <angelo030825@gmail.com>
2025-10-17 15:19:08 -0300

    refatorando os projetos

--- src/constants/projects.jsx
+++ src/constants/projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Peace from "../assets/img/peace_project.jpg";
 import Poliverso from "../assets/img/poliverso.jpg";
+import Tabnews from "../assets/img/tabnews.jpg";
 
 function Projects() {
   return (
@@ -13,9 +14,9 @@ function Projects() {
         </div>
         <div className="flex gap-[45px]">
           {/* peace */}
-          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[15px_15px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
+          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[12px_12px_12px_0px_rgba(0,0,0,0.25)]">
             <img
-              className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
+              className="w-80 h-56 mx-5 mt-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
               src={Peace}
               alt=""
             />
@@ -26,13 +27,18 @@ function Projects() {
               <div className="mx-4 my-1 rounded-[5px] w-min px-2.5 bg-[#0000ff]">
                 <p className="font-satoshi text-white">#Mobile</p>
               </div>
+              <p className="px-5 font-satoshi">
+                  Peace é um aplicativo para iOS que desenvolvi com o objetivo
+                  de ajudar as pessoas a se organizarem melhor, gerenciarem suas
+                  tarefas e acompanharem sua rotina de forma prática.
+              </p>
             </div>
           </div>
 
           {/* poliverso */}
-          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[15px_15px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
+          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[12px_12px_12px_0px_rgba(0,0,0,0.25)]">
             <img
-              className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
+              className="w-80 h-56 mx-5 mt-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
               src={Poliverso}
               alt=""
             />
@@ -43,23 +49,30 @@ function Projects() {
               <div className="mx-4 my-1 rounded-[5px] w-min px-2.5 bg-[#0000ff]">
                 <p className="font-satoshi text-white">#Web</p>
               </div>
+              <p className="px-5 font-satoshi">
+                A Poliverso é uma agencia do audiovisual que que conecta
+                talentos para produzir conteúdos de alta qualidade.
+              </p>
             </div>
           </div>
 
           {/* outro projeto */}
-          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[15px_15px_15px_0px_rgba(0,0,0,0.25)] outline-[10px] outline-neutral-800">
+          <div className="w-91 h-[480px] mt-8 bg-white rounded-[20px] shadow-[12px_12px_12px_0px_rgba(0,0,0,0.25)]">
             <img
-              className="w-80 h-56 m-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
-              src={Poliverso}
+              className="w-80 h-56 mx-5 mt-5 rounded-[5px] shadow-[4px_4px_10px_1px_rgba(0,0,0,0.25)]"
+              src={Tabnews}
               alt=""
             />
             <div>
               <h2 className="pl-5 text-4xl text-[#0000ff] font-instrument italic">
-                estante.me
+                clone-tabnews
               </h2>
               <div className="mx-4 my-1 rounded-[5px] w-min px-2.5 bg-[#0000ff]">
-                <p className="font-satoshi text-white">#Web</p>
+                <p className="font-satoshi text-white">#FullStack</p>
               </div>
+              <p className="px-5 font-satoshi">
+                O clone do tabnews é um projeto que foi desenvolvido no curso.dev. Nele aprendi a criar testes automatizados, autenticação de usuários e deploy.
+              </p>
             </div>
           </div>
         </div>
