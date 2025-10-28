f80c749d39fc00f12d475e8edd8cab4fc3687626
Angelo Mendes <angelo030825@gmail.com>
2025-10-28 00:56:57 -0300

    refatorando `App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ function App() {
   return (
     <>
       <div id="smooth-wrapper" className="flex w-full min-h-screen p-6 sticky">
-        <div id="smooth-content" className="bg-white rounded-2xl flex flex-col w-full p-20">
+        <div
+          id="smooth-content"
+          className="bg-white rounded-2xl flex flex-col w-full p-20"
+        >
           <div className="justify-end">
             <div>
-              <h1 className="text-[#0000FF] text-4xl lg:text-8xl font-instrument italic font-medium">Hi folks!</h1>
+              <h1 className="text-[#0000FF] text-4xl lg:text-8xl font-instrument italic font-medium">
+                Hi folks!
+              </h1>
             </div>
 
             <div className="flex justify-end">
-              <h2 className="text-[#0000FF] text-2xl lg:text-5xl font-instrument italic font-medium">About me</h2>
+              <h2 className="text-[#0000FF] text-2xl lg:text-5xl font-instrument italic font-medium">
+                About me
+              </h2>
             </div>
 
             <div className="flex justify-end">
-              <p className="w-[100%] lg:w-[549px] text-end pt-[10px] font-satoshi">
+              <p className="w-full lg:w-[549px] text-end pt-2.5 font-satoshi">
                 Sou estudante de Análise e Desenvolvimento de Sistemas, com
                 especialização em desenvolvimento Web e Mobile. Possuo
                 experiência prática em projetos freelance, onde desenvolvi
@@ -30,15 +37,23 @@ function App() {
 
             <div className="flex justify-between gap-28">
               <div>
-                <h2 className="text-[#0000FF] text-2xl lg:text-5xl font-instrument italic font-medium">Skills</h2>
-                <div className="pt-[10px]">
+                <h2 className="text-[#0000FF] text-2xl lg:text-5xl font-instrument italic font-medium">
+                  Skills
+                </h2>
+                <div className="pt-2.5">
                   <p className="font-satoshi">
                     Frontend: React.js, JavaScript, TailwindCSS, HTML, CSS
                   </p>
                   <p className="font-satoshi">Backend: Node.js, Python, Java</p>
-                  <p className="font-satoshi">Mobile: Swift, SwiftUI, ViewCode, UIKit</p>
-                  <p className="font-satoshi">Banco de Dados: SQL, NoSQL (MongoDB)</p>
-                  <p className="font-satoshi">Outros: Git, GitHub, pacote office</p>
+                  <p className="font-satoshi">
+                    Mobile: Swift, SwiftUI, ViewCode, UIKit
+                  </p>
+                  <p className="font-satoshi">
+                    Banco de Dados: SQL, NoSQL (MongoDB)
+                  </p>
+                  <p className="font-satoshi">
+                    Outros: Git, GitHub, pacote office
+                  </p>
                 </div>
               </div>
               {/* <div className="w-[200px] h-[180px] relative top-20 right-40 rotate-[227.58deg] bg-[#0000ff] blur-[100px]" /> */}
